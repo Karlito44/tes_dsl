f92819de97086f087853e13bfb88c92a1d4a5752
Karlito44 <kamal.aarab@hotmail.fr>
2020-06-04 15:15:03 +0200

    Nouveau DSL : Git

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 		getName(){
 			# delimiter = "/"
-			file="$(find . -name "*.mydsl")"
+			file="$(find ../ -name "*.mydsl")"
 			# name = $file
 			name=$(echo $file | tr "./" " ")
 			for word in $name
@@ -11,10 +11,9 @@
 				done
 		}
 		msg=$(getName)
-		echo $msg
 		# git init
 		git add .
 		git commit -m "$msg"
-		# git remote add origin https://github.com/Karlito44/tes_dsl
+		git remote add origin https://github.com/Karlito44/tes_dsl
 		# git push -f --set-upstream origin master
 		git push -f
